e4c32b213349c9bd129325407ce4cdc2048b5dfd
PJ-Turner <70144724+PJ-Turner@users.noreply.github.com>
2022-09-08 14:13:07 +0200

    Add files via upload

---
+++ Project2.1_31655815/Project2.1_31655815/Authentication/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2._1_31655815
+{
+    public class ApplicationUser
+    {
+    }
+}
